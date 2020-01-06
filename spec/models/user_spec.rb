6282d7c68f2ee2fa53d3f75ad3d5dcf66674cacc
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-06 12:35:18 +0900

    Add user name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it '無効なnameでの登録' do
-      user.build(:user, name: "")
+      user = build(:user, name: "")
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:name]).to include("can't be blank")
     end
 
     it '無効なemailでの登録' do
-      user.build(:user, email: "")
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
