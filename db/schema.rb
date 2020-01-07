f43934aec0c4d28d4626f1924d633a5dcf803247
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-07 12:03:39 +0900

    Modify migration to group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_030153) do
+ActiveRecord::Schema.define(version: 2020_01_07_022712) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
