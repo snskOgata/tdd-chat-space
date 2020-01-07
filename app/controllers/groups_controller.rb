15e3599740fe7a3f0d6fd23a023ca87a8dfef09a
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-07 15:04:18 +0900

    Add groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,18 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
+
+  def create
+    @group = Group.create(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:name, user_ids: [])
+    end
 end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,17 +2,10 @@
 require 'rails_helper'
 
 describe GroupsController do
-  describe 'GET #index' do
-    it "グループに紐ついた変数" do
-      user = create(:user)
-      group = create(:group)
-      get :index
-      expect(assigns(:group).name).to eq(group.name)
-    end
-
-    it "renders the :index template" do
-      get :index
-      expect(response).to render_template :index
+  describe 'GET #new' do
+    it "newのテンプレート比較" do
+      # get :new
+      # expect(response).to render_template :new
     end
   end
 end
