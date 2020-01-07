5eb22401b998d4bd21877d03fcfd5a19e714357c
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-07 17:18:24 +0900

    Add view to groups#edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :messages, only: :index
-  resources :groups, only: [:index, :new, :create, :edit]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
