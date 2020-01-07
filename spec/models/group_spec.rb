5de8a3bff2b25a8e6f52d79ff1eda0ae1bd9cdcd
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-07 12:28:01 +0900

    Add group model test

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Group do
+  describe '#create' do
+    it 'nameが空での登録' do
+      group = build(:group, name: "")
+      group.valid?
+      expect(group.errors[:name]).to include("")
+    end
+
+    it 'nameが"   "での登録' do
+      group = build(:group, name: "   ")
+      group.valid?
+      expect(group.errors[:name]).to include("")
+    end
+
+    it 'nameを31字での登録' do
+      group = build(:group, name: "a"*31)
+      group.valid?
+      expect(group.errors[:name]).to include("")
+    end
+
+
+    it 'nameを30字での登録' do
+      group = build(:group, name: "a"*30)
+      expect(group).to be_valid
+    end
+  end
+end
