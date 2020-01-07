a8cc4173564c53c52937ba35bb13a16779b6f52e
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-07 17:04:28 +0900

    Add test to groups#new

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,11 +1,46 @@
-
 require 'rails_helper'
 
 describe GroupsController do
-  describe 'GET #new' do
-    it "newのテンプレート比較" do
-      # get :new
-      # expect(response).to render_template :new
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'ログイン状態' do
+      before do
+        login user
+      end
+      it 'indexにアクセス' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    context '非ログイン状態' do
+      it 'indexにアクセス→ログイン画面へ' do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#new' do
+
+    context 'ログイン状態' do
+      before do
+        login user
+      end
+      it 'newにアクセス' do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    context '非ログイン状態' do
+      it 'indexにアクセス→ログイン画面へ' do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
